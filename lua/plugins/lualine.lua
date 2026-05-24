bf737892c1d7c38ac9be56c929235b24a98b23e9
GodLight <godlightdsu@gmail.com>
2026-05-25 07:00:11 +0800

    feat: 添加 lualine 美觀的狀態列，並將註解翻譯為繁體中文

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,74 @@
+-- 美觀的狀態列
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		-- 呼叫 setup 並設定 lualine 的外觀參數
+		require("lualine").setup({
+			options = {
+				-- 若有安裝 nerdfont，顯示檔案圖示
+				icons_enabled = true,
+				-- 設定主題為 kanagawa_paper（可依需求選擇其他主題）
+				theme = "kanagawa_paper",
+				-- 使用分隔符將欄位區隔開
+				component_separators = { left = "", right = "" },
+				-- 使用三角分隔區塊
+				section_separators = { left = "", right = "" },
+				-- 在指定檔案類型中停用 statusline 與 winbar
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				-- 不在 NvimTree 視窗上啟用 lualine 的 focus
+				ignore_focus = { "NvimTree" },
+				-- 在中間分割 lualine 區域
+				always_divide_middle = true,
+				-- 關閉全域狀態列（僅在需要時啟用）
+				globalstatus = false,
+				-- 每 1000 毫秒更新一次顯示
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			-- Setup what each lualine section will contain
+			-- 設定各區段內容（左側為 a，右側為 z）
+			sections = {
+				-- 在 a 區顯示目前的編輯模式
+				lualine_a = { "mode" },
+				-- 在 b 區顯示 git 分支、差異與診斷資訊
+				lualine_b = { "branch", "diff", "diagnostics" },
+				-- 在 c 區顯示檔名
+				lualine_c = { "filename" },
+				-- 在 x 區顯示檔案編碼、格式與檔案類型
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				-- 在 y 區顯示目前在檔案中的進度
+				lualine_y = { "progress" },
+				-- 在 z 區顯示游標的精確位置
+				lualine_z = { "location" },
+			},
+			-- Setup what each section will contain in inactive buffers
+			inactive_sections = {
+				-- 在非啟用視窗中，a 與 b 區留空
+				lualine_a = {},
+				lualine_b = {},
+				-- 在 c 區顯示檔名
+				lualine_c = { "filename" },
+				-- 在 x 區顯示游標位置
+				lualine_x = { "location" },
+				-- 在 y 與 z 區留空
+				lualine_y = {},
+				lualine_z = {},
+			},
+			-- 使用預設設定作為 tabline、winbar、inactive winbar 與 extensions
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
+		})
+	end,
+}
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,53 +5,53 @@ return {
   version = '*', 
   dependencies = {
     'nvim-lua/plenary.nvim',
-    -- optional but recommended
+    -- 可選但建議安裝（提供更快的 fzf 效能）
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
     -- 使用望遠鏡內建功能
     local builtin = require('telescope.builtin')
-    local keymap = vim.keymap
-    -- 設定 vim 快速鍵 <空白鍵> + f + f，按檔案名稱搜尋檔案。
+    local keymap = vim.keymap  -- <leader> 是空格鍵，這裡用於觸發搜尋功能
+    -- 依檔名搜尋檔案
     keymap.set('n', '<leader>ff', builtin.find_files, {desc = "[F]ind [F]iles"})
-    -- 設定 vim 快速鍵 <空白鍵> + f + g，按檔案內容搜尋檔案。
+    --依內容搜尋檔案
     keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "[F]ind by [G]rep"})
-    -- 設定 vim 快速鍵 <空白鍵> + f + d，搜尋程式碼診斷資訊。
+    -- 搜尋診斷資訊
     keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-    -- 設定 vim 快速鍵 <空白鍵> + f + r，恢復上一次的搜尋。
+    -- 恢復上一次的搜尋
     keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]inder [R]esume' })
-    -- 設定 vim 快速鍵 <空白鍵> + f + .，搜尋最近使用的檔案。
+    -- 搜尋最近使用的檔案
     keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-    -- 設定 vim 快速鍵 <空白鍵> + f + b，搜尋開啟的緩衝區。
+    -- 搜尋開啟的緩衝區
     keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind Existing [B]uffers' })
   end
 },
-{ -- telescope-ui-select
+{ -- telescope-ui-select 擴充
   'nvim-telescope/telescope-ui-select.nvim',
   config = function()
-    -- get access to telescopes navigation functions
+    -- 取得 Telescope 的導覽相關函式
     local actions = require("telescope.actions")
     require("telescope").setup({
-      -- use ui-select dropdown as our ui
+      -- 使用 ui-select 的下拉樣式作為介面
       extensions = {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown {}
         }
       },
-      -- set keymappings to navigate through items in the telescope io
+      -- 設定快捷鍵以在 Telescope 列表中導航
       mappings = {
         i = {
-          -- use <cltr> + n to go to the next option
+          -- 使用 <Ctrl>+n 移到下一項
           ["<C-n>"] = actions.cycle_history_next,
-          -- use <cltr> + p to go to the previous option
+          -- 使用 <Ctrl>+p 移到前一項
           ["<C-p>"] = actions.cycle_history_prev,
-          -- use <cltr> + j to go to the next preview
+          -- 使用 <Ctrl>+j 移到下一個預覽項目
           ["<C-j>"] = actions.move_selection_next,
-          -- use <cltr> + k to go to the previous preview
+          -- 使用 <Ctrl>+k 移到上一個預覽項目
           ["<C-k>"] = actions.move_selection_previous,
         }
       },
-      -- load the ui-select extension
+      -- 載入 ui-select 擴充
       require("telescope").load_extension("ui-select")
     })
   end
